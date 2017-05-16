ac7c76c55409f2eb72b771564f4ce6ac052b2e83
songshaoyu <ahu8756@gmail.com>
2017-05-16 21:27:48 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :cart_items
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   root 'products#index'
 

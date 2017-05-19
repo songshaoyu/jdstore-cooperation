d20efb2d1cc7e6545a15d74bd7833dc3d7020b7a
songshaoyu <ahu8756@gmail.com>
2017-05-19 22:07:13 +0800

    users can see their orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
+    before_create :generate_token
     belongs_to :user
+
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
@@ -7,4 +9,8 @@ class Order < ApplicationRecord
   validates :shipping_address, presence: true
 
   has_many :product_lists
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end

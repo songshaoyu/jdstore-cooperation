e160f28c22eff910f8ff452e7328ec541ffccfb7
shaoyu <571398414@qq.com>
2017-06-05 18:52:29 +0800

    实现放大镜

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,25 +47,25 @@ $(function(){
    }
 
    //预览图
-   $(document).on('mouseover', '.intro-preview-item', function () {
-  var src = $(this).find('img').attr('src')
-  $('.intro-bigPic img').attr('src', src)
-    $('#thumb-lens').css('background-image', 'url(' + src + ')')
-  $(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
-  m.attach({
-        thumb: '#thumb',
-        large: src,
-        largeWrapper: 'preview'
-  })
-})
-
-$('.intro-preview-activeItem').trigger('mouseover')
-$(document).on('mouseover', '.magnifier-thumb-wrapper', function (e) {
-    $('#preview').css('visibility', 'visible')
-})
-$(document).on('mouseout', '.magnifier-thumb-wrapper', function (e) {
-    $('#preview').css('visibility', 'hidden')
-})
+//    $(document).on('mouseover', '.intro-preview-item', function () {
+//   var src = $(this).find('img').attr('src')
+//   $('.intro-bigPic img').attr('src', src)
+//     $('#thumb-lens').css('background-image', 'url(' + src + ')')
+//   $(this).addClass('intro-preview-activeItem').siblings().removeClass('intro-preview-activeItem')
+//   m.attach({
+//         thumb: '#thumb',
+//         large: src,
+//         largeWrapper: 'preview'
+//   })
+// })
+//
+// $('.intro-preview-activeItem').trigger('mouseover')
+// $(document).on('mouseover', '.magnifier-thumb-wrapper', function (e) {
+//     $('#preview').css('visibility', 'visible')
+// })
+// $(document).on('mouseout', '.magnifier-thumb-wrapper', function (e) {
+//     $('#preview').css('visibility', 'hidden')
+// })
 //fangdajing
 // 放大镜
 // $('#preview').css('visibility', 'hidden')

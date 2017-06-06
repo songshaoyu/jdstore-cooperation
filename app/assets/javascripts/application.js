c8a1ca7e1aa0978510a94a5b221b8f4cf6dc5d45
shaoyu <571398414@qq.com>
2017-06-06 11:37:41 +0800

    尝试购物车动画

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -181,3 +181,30 @@ function MouseEvent(e) {
   this.x = e.pageX;
   this.y = e.pageY;
 };
+
+// addCart
+
+$(function() {
+    var offset = $("#end").offset();
+    $(".addCartBtn").click(function(event){
+        var addcar = $(this);
+        var img = addcar.closest('.productList-item').find('.productList-item-pic img').attr('src');
+        if (!img) img = $('#thumb').attr('src');
+        var flyer = $('<img class="u-flyer" src="'+img+'">');
+        flyer.fly({
+            start: {
+                left: event.pageX - 20, //开始位置（必填）#fly元素会被设置成position: fixed
+                top: event.clientY - 20 //开始位置（必填）
+            },
+            end: {
+                left: $('.fa-shopping-cart').offset().left, //结束位置（必填）
+                top: $('.fa-shopping-cart').offset().top - $(window).scrollTop(), //结束位置（必填）
+                width: 10, //结束时宽度
+                height: 10 //结束时高度
+            },
+            onEnd: function(){ //结束回调
+                this.destory(); //移除dom
+            }
+        });
+    });
+});
